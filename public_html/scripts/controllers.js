d17ba00d7651c89a668aded7d4b71f11353f229b
Diego Faro <diegop.pin@gmail.com>
2017-03-12 00:14:33 +0000

    restaurant web with data hardcoded

---
+++ public_html/scripts/controllers.js
@@ -0,0 +1,112 @@
+'use strict';
+
+angular.module('confusionApp')
+
+        .controller('MenuController', ['$scope', 'menuFactory', function ($scope, menuFactory) {
+
+                $scope.tab = 1;
+                $scope.filtText = '';
+                $scope.showDetails = false;
+
+                $scope.dishes = menuFactory.getDishes();
+
+
+                $scope.select = function (setTab) {
+                    $scope.tab = setTab;
+
+                    if (setTab === 2) {
+                        $scope.filtText = "appetizer";
+                    } else if (setTab === 3) {
+                        $scope.filtText = "mains";
+                    } else if (setTab === 4) {
+                        $scope.filtText = "dessert";
+                    } else {
+                        $scope.filtText = "";
+                    }
+                };
+
+                $scope.isSelected = function (checkTab) {
+                    return ($scope.tab === checkTab);
+                };
+
+                $scope.toggleDetails = function () {
+                    $scope.showDetails = !$scope.showDetails;
+                };
+            }])
+
+        .controller('ContactController', ['$scope', function ($scope) {
+
+                $scope.feedback = {mychannel: "", firstName: "", lastName: "", agree: false, email: ""};
+
+                var channels = [{value: "tel", label: "Tel."}, {value: "Email", label: "Email"}];
+
+                $scope.channels = channels;
+                $scope.invalidChannelSelection = false;
+
+            }])
+
+        .controller('FeedbackController', ['$scope', function ($scope) {
+
+                $scope.sendFeedback = function () {
+
+                    console.log($scope.feedback);
+
+                    if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
+                        $scope.invalidChannelSelection = true;
+                        console.log('incorrect');
+                    } else {
+                        $scope.invalidChannelSelection = false;
+                        $scope.feedback = {mychannel: "", firstName: "", lastName: "", agree: false, email: ""};
+                        $scope.feedback.mychannel = "";
+                        $scope.feedbackForm.$setPristine();
+                        console.log($scope.feedback);
+                    }
+                };
+            }])
+
+        .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function ($scope, $stateParams, menuFactory) {
+                console.log($stateParams.id)
+                var dish = menuFactory.getDish(parseInt($stateParams.id, 10));
+                console.log('1111')
+                console.log($scope.feedback)
+                $scope.dish = dish;
+
+            }])
+
+        .controller('DishCommentController', ['$scope', function ($scope) {
+
+                $scope.feedback = {star: 5, comments: "", name: "", date: ""};
+
+                $scope.submitComment = function () {
+
+                    //Step 2: This is how you record the date
+                    $scope.feedback.date = new Date().toISOString();
+
+                    // Step 3: Push your comment into the dish's comment array
+                    $scope.dish.comments.push({rating: $scope.feedback.star, comment: $scope.feedback.comments, author: $scope.feedback.name, date: $scope.feedback.date});
+
+                    $scope.commentForm.$setPristine();
+
+                    $scope.feedback = {star: 5, comments:"", name:"", date:"" };
+                }
+            }])
+
+        // implement the IndexController and About Controller here
+        .controller('IndexController', ['$scope', 'menuFactory', 'corporateFactory', function ($scope, menuFactory, corporateFactory) {
+
+                var dish = menuFactory.getDish(0);
+                $scope.dish = dish;
+
+                $scope.promotion = menuFactory.getPromotion(0);
+
+                $scope.leader = corporateFactory.getLeader(3);
+
+            }])
+
+        .controller('AboutController', ['$scope', 'corporateFactory', function ($scope, corporateFactory) {
+
+                $scope.corpfac = corporateFactory.getLeaders();
+            }])
+        ;
+
+
